99960d9dee430b88b734847b5bb03fe7ca841d05
Daniel <daniel.dziubecki@wp.pl>
2017-09-23 13:06:26 +0200

    removing referenced Sagas

--- Order.Service/Startup.cs
+++ Order.Service/Startup.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Reflection;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
 using Automatonymous;
